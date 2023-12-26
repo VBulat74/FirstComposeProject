239378958ddda2b7c2cd74e9a169e98e700f3148
VB <Vladimir.Bulat@gmail.com>
2023-12-26 11:10:16 +0300

    Lesson 2.8

--- app/src/main/java/ru/com/vbulat/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/ru/com/vbulat/firstcomposeproject/MainActivity.kt
@@ -3,18 +3,15 @@ package ru.com.vbulat.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import ru.com.vbulat.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import ru.com.vbulat.firstcomposeproject.ui.theme.InstagramProfileCard
 
@@ -38,11 +35,5 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun TestText(){
-    Text(
-        text = "Hello world!",
-        fontSize = 24.sp,
-        fontWeight = FontWeight.ExtraBold,
-        fontFamily = FontFamily.Serif,
-        textDecoration = TextDecoration.Underline
-    )
+    Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "")
 }
--- app/src/main/java/ru/com/vbulat/firstcomposeproject/ui/theme/InstogramProfileCard.kt
+++ app/src/main/java/ru/com/vbulat/firstcomposeproject/ui/theme/InstogramProfileCard.kt
@@ -1,9 +1,8 @@
 package ru.com.vbulat.firstcomposeproject.ui.theme
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,12 +17,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import ru.com.vbulat.firstcomposeproject.R
 
 @Composable
 fun InstagramProfileCard(){
@@ -40,10 +40,12 @@ fun InstagramProfileCard(){
             horizontalArrangement = Arrangement.SpaceEvenly,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Box (
+            Image(
                 modifier = Modifier
-                    .size(50.dp)
-                    .background(color = Color.Red),
+                    .size(50.dp),
+                painter = painterResource(id = R.drawable.ic_instagram),
+                contentDescription = "",
+
             )
 
             UserStatistics(title = "Post", value = "6.950")
