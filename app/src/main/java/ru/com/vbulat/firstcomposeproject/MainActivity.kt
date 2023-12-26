358b1d042add8e74ee7b137a58e23235a686bbb8
VB <Vladimir.Bulat@gmail.com>
2023-12-26 10:34:47 +0300

    Lesson 2.7

--- app/src/main/java/ru/com/vbulat/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/ru/com/vbulat/firstcomposeproject/MainActivity.kt
@@ -7,7 +7,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import ru.com.vbulat.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import ru.com.vbulat.firstcomposeproject.ui.theme.InstagramProfileCard
 
@@ -26,4 +33,16 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun TestText(){
+    Text(
+        text = "Hello world!",
+        fontSize = 24.sp,
+        fontWeight = FontWeight.ExtraBold,
+        fontFamily = FontFamily.Serif,
+        textDecoration = TextDecoration.Underline
+    )
 }
--- app/src/main/java/ru/com/vbulat/firstcomposeproject/ui/theme/InstogramProfileCard.kt
+++ app/src/main/java/ru/com/vbulat/firstcomposeproject/ui/theme/InstogramProfileCard.kt
@@ -7,18 +7,23 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun InstagramProfileCard(){
@@ -41,31 +46,32 @@ fun InstagramProfileCard(){
                     .background(color = Color.Red),
             )
 
-            TwoBoxes()
-            TwoBoxes()
-            TwoBoxes()
+            UserStatistics(title = "Post", value = "6.950")
+            UserStatistics(title = "Followers", value = "436M")
+            UserStatistics(title = "Following", value = "76")
         }
     }
 }
 
 @Composable
-private fun TwoBoxes(){
+private fun UserStatistics(
+    title: String,
+    value: String,
+){
     Column (
         modifier = Modifier
-            .size(60.dp),
+            .height(80.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ){
-        Box (
-            modifier = Modifier
-                .size(25.dp)
-                .background(color = Color.Green),
+        Text(
+            text = value,
+            fontSize = 24.sp,
+            fontFamily = FontFamily.Cursive
         )
-
-        Box (
-            modifier = Modifier
-                .size(25.dp)
-                .background(color = Color.Blue),
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold
         )
     }
 }
