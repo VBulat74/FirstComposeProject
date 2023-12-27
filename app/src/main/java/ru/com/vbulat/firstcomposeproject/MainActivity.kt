108245681364780b3718033094006519ae50fc36
VB <Vladimir.Bulat@gmail.com>
2023-12-27 16:45:57 +0300

    Lesson 4.2

--- app/src/main/java/ru/com/vbulat/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/ru/com/vbulat/firstcomposeproject/MainActivity.kt
@@ -3,26 +3,19 @@ package ru.com.vbulat.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModelProvider
 import ru.com.vbulat.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import ru.com.vbulat.firstcomposeproject.ui.theme.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         setContent {
             FirstComposeProjectTheme() {
                 Box (
@@ -30,32 +23,9 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize()
                         .background(color = MaterialTheme.colorScheme.background),
                 ) {
-                    InstagramProfileCard()
+                    InstagramProfileCard(viewModel = viewModel)
                 }
             }
         }
     }
-}
-
-@Preview
-@Composable
-fun TestText(){
-    Box (
-        modifier = Modifier
-            .size(200.dp)
-            .background(Color.Cyan),
-    ){
-        Image(
-            modifier = Modifier
-                .background(Color.Green)
-                .padding(25.dp)
-                .size(100.dp)
-                .background(Color.Red)
-            ,
-            painter = ColorPainter(Color.Yellow),
-            contentDescription = "",
-            contentScale = ContentScale.FillHeight
-        )
-    }
-
 }
--- app/src/main/java/ru/com/vbulat/firstcomposeproject/ui/theme/InstogramProfileCard.kt
+++ app/src/main/java/ru/com/vbulat/firstcomposeproject/ui/theme/InstogramProfileCard.kt
@@ -13,11 +13,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -25,13 +28,18 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import ru.com.vbulat.firstcomposeproject.MainViewModel
 import ru.com.vbulat.firstcomposeproject.R
 
 @Composable
-fun InstagramProfileCard(){
+fun InstagramProfileCard(
+    viewModel: MainViewModel
+){
+
+    val isFollowed : State<Boolean> = viewModel.isFollowing.observeAsState(false)
+
     Card (
         modifier = Modifier.padding(8.dp),
         shape = RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp),
@@ -77,11 +85,37 @@ fun InstagramProfileCard(){
                 text = "www.facebook.com/emotional_health",
                 fontSize = 14.sp,
             )
-
-            Button(onClick = {}) {
-                Text(text = "Follow")
+            FollowButton(isFollowed = isFollowed.value) {
+                viewModel.changeFollowingStatus()
             }
         }
+    }
+}
+
+@Composable
+private fun FollowButton(
+    isFollowed : Boolean,
+    clickListener : () -> Unit,
+){
+    Button(
+        onClick = {
+            clickListener()
+        },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = if (isFollowed) {
+                MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
+            } else {
+                MaterialTheme.colorScheme.primary
+            }
+        ),
+    ) {
+        val text = if (isFollowed){
+            "Unfollow"
+        } else {
+            "Follow"
+        }
+
+        Text(text = text)
     }
 }
 
@@ -105,20 +139,5 @@ private fun UserStatistics(
             text = title,
             fontWeight = FontWeight.Bold
         )
-    }
-}
-
-@Preview()
-@Composable
-fun InstagramProfileCardPreviewDay(){
-    FirstComposeProjectTheme (darkTheme = false) {
-        InstagramProfileCard()
-    }
-}
-@Preview()
-@Composable
-fun InstagramProfileCardPreviewNight(){
-    FirstComposeProjectTheme (darkTheme = true) {
-        InstagramProfileCard()
     }
 }
