96671bfe002d0f7075b175f69db0a59d52e16030
VB <Vladimir.Bulat@gmail.com>
2023-12-28 17:05:32 +0300

    Lesson 4.6

--- app/src/main/java/ru/com/vbulat/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/ru/com/vbulat/firstcomposeproject/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
 import ru.com.vbulat.firstcomposeproject.ui.theme.FirstComposeProjectTheme
@@ -17,12 +19,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         setContent {
-            FirstComposeProjectTheme() {
-                Box (
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(color = MaterialTheme.colorScheme.background),
-                ) {
+            Test(viewModel = viewModel)
+        }
+    }
+}
+
+@Composable
+fun Test(viewModel: MainViewModel){
+    FirstComposeProjectTheme() {
+        Box (
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = MaterialTheme.colorScheme.background),
+        ) {
+            LazyColumn(
+            ) {
+                items(500) {
                     InstagramProfileCard(viewModel = viewModel)
                 }
             }
